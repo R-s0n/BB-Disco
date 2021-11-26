cfecbd97b1c85b1e7d37b64165bd55eb2f4a7e6a
R-s0n <rs0n@oswe-box.seawallsecurity.com>
2021-11-26 12:14:26 -0600

    Add bugcrowd

--- bbdisco.py
+++ bbdisco.py
@@ -11,6 +11,14 @@ class HackerOne:
         self.mrp = mrp
         self.link = f"https://hackerone.com/{mrp}"
 
+class BugCrowd:
+    def __init__(self, mrp=None):
+        self.url = "https://bugcrowd.com/programs"
+        self.class_ = "cc-inline-clamp-2"
+        self.platform = "BugCrowd"
+        self.mrp = mrp
+        self.link = f"https://bugcrowd.com/{mrp}"
+
 class MostRecentPrograms:
     def __init__(self, hackerone, bugcrowd, intigriti):
         self.hackerone = hackerone
@@ -34,7 +42,7 @@ def send_init_notification():
     message_json = {'text':':bulb::bulb:  Bug Bounty Program Monitoring Server Online!  :bulb::bulb:','username':'BB-Disco','icon_emoji':':bug:'}
     f = open(f'{home_dir}/.keys/slack_web_hook')
     token = f.read()
-    requests.post(f'{token}', json=message_json)
+    # requests.post(f'https://hooks.slack.com/services/{token}', json=message_json)
 
 def send_slack_notification(program):
     get_home_dir = subprocess.run(["echo $HOME"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True, shell=True)
@@ -44,7 +52,7 @@ def send_slack_notification(program):
     f = open(f'{home_dir}/.keys/slack_web_hook')
     token = f.read()
     print(f"[+] New Program Found!  Name: {program.mrp}")
-    # requests.post(f'{token}', json=message_json)
+    # requests.post(f'https://hooks.slack.com/services/{token}', json=message_json)
 
 def hackerone_check(mrps):
     program = HackerOne(mrps.hackerone)
@@ -59,16 +67,36 @@ def hackerone_check(mrps):
     else:
         now = datetime.now()
         formatted_now = now.strftime("%m/%d/%Y, %H:%M:%S")
-        print(f"[-] Same -- {mrp} -- {formatted_now}")
+        print(f"[-] HackerOne -- Same -- {mrp} -- {formatted_now}")
+        return mrps
+
+def bugcrowd_check(mrps):
+    program = BugCrowd(mrps.bugcrowd)
+    mrpo = get_most_recent_program_obj(program)
+    mrp = mrpo.text
+    if mrp != mrps.bugcrowd:
+        print(f"[!] New BugCrowd Program Found!  {mrp}")
+        new_program = BugCrowd(mrp)
+        send_slack_notification(new_program)
+        mrps.bugcrowd = mrp
+        return mrps
+    else:
+        now = datetime.now()
+        formatted_now = now.strftime("%m/%d/%Y, %H:%M:%S")
+        print(f"[-] BugCrowd -- Same -- {mrp} -- {formatted_now}")
         return mrps
 
 def get_init_programs():
     hackerone = HackerOne()
-    mrpo = get_most_recent_program_obj(hackerone)
-    mrp = mrpo.text
-    print(f"[-] HackerOne -- Initial Program -- {mrp}")
+    mrpo_hackerone = get_most_recent_program_obj(hackerone)
+    mrp_hackerone = mrpo_hackerone.text
+    print(f"[-] HackerOne -- Initial Program -- {mrp_hackerone}")
+    bugcrowd = BugCrowd()
+    mrpo_bugcrowd = get_most_recent_program_obj(bugcrowd)
+    mrp_bugcrowd = mrpo_bugcrowd.text
+    print(f"[-] BugCrowd -- Initial Program -- {mrp_bugcrowd}")
     send_init_notification()
-    return MostRecentPrograms(mrp, "", "")
+    return MostRecentPrograms(mrp_hackerone, mrp_bugcrowd, "")
 
 def arg_parse():
     parser = argparse.ArgumentParser()
@@ -80,6 +108,7 @@ def main(args):
     mrps = get_init_programs()
     while True:
         mrps = hackerone_check(mrps)
+        mrps = bugcrowd_check(mrps)
         sleep(10)
 
 if __name__ == "__main__":
